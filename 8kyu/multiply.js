ef32b8e564122254b887550c44bd00c6a5358ab8
Muhammad Reffy <syahnataa@gmail.com>
2024-12-05 09:13:43 +0700

    This code does not execute properly. Try to figure out why

---
+++ 8kyu/multiply.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+// This code does not execute properly. Try to figure out why.
+
+// First Solution
+function multiply(i, j) {
+  return i * j;
+}
+
+console.log(multiply(5, 2));
+
+// Second Solution
+const multiply = (i, j) => i * j;
+
+console.log(multiply(2, 5));
