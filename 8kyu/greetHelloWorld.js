b492788da8306a4098a34f7248836e315fce4170
Muhammad Reffy <syahnataa@gmail.com>
2024-12-06 10:13:19 +0700

    greet function hello world

---
+++ 8kyu/greetHelloWorld.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// First solution
+function greet() {
+  return "hello world!";
+}
+
+// Second solution
+const greet = () => "hello world!";
